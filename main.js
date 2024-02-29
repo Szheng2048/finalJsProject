8a4b5223bd654092cb52bf739da219aa58f416b1
Steven Zheng <szheng.persevere@gmail.com>
2024-02-29 13:39:05 -0600

    added the dieroll and made the arrays, currently trying to figure out how to make the reroll function

---
+++ main.js
@@ -0,0 +1,61 @@
+//*variables***********/
+const rollButton = document.getElementById("rollButton")
+const rerollButton = document.getElementById('reRollButton')
+const rollingDice = document.querySelectorAll(".rollingDice")
+
+let d6Array = []
+let reRollArray = []
+let conditionCount = 0
+//count of 0 is for the first roll
+//count of 1 is for the reroll
+//amount of times the dice is selecter after roll
+
+//dice rolling function**/
+
+
+
+
+
+//*eventListeners********/
+
+rollButton.addEventListener("click",()=>{
+    if(conditionCount === 0){
+        d6Array.push(randomD6result(),randomD6result(),randomD6result(),randomD6result(),randomD6result(),randomD6result())
+        console.log(d6Array)
+        for(let i = 0;i<rollingDice.length;i++){
+            rollingDice[i].src= `diceImages/dice-face-${d6Array[i]}.png`
+        }
+        conditionCount = 1
+    }
+})
+
+for(let individualDice of rollingDice){
+    individualDice.addEventListener('click',(event)=>{
+        let timeSelected = 0
+        if(conditionCount === 1){
+            if(timeSelected === 0){
+                reRollArray.push(event.target.src.slice(46,-4))
+                console.log(reRollArray)
+                timeSelected = 1
+            }
+        }
+    })
+}
+
+
+
+
+
+//*******logic functions */
+function randomD6result(){
+    return Math.ceil(Math.random()*6)
+}
+
+
+//true or false functions
+
+
+
+
+
+//**reset function */
