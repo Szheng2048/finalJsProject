635c3e8bd786806026ab269f2182d6e982a192ab
Steven Zheng <szheng.persevere@gmail.com>
2024-03-01 00:12:02 -0600

    completed logical conditioning and removed errors from the dice rolling program

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ let reRollArray = []
 let conditionCount = 0
 //count of 0 is for the first roll
 //count of 1 is for the reroll
+let rerollCount = 0
+//as long as rerollCount is not zero reroll can be clicked
+let maxRerollCount = 0
 //amount of times the dice is selecter after roll
 
 //dice rolling function**/
@@ -20,7 +23,7 @@ let conditionCount = 0
 
 rollButton.addEventListener("click",()=>{
     if(conditionCount === 0){
-        d6Array.push(randomD6result(),randomD6result(),randomD6result(),randomD6result(),randomD6result(),randomD6result())
+        d6Array.push(randomD6result(),randomD6result(),randomD6result(),randomD6result(),randomD6result())
         console.log(d6Array)
         for(let i = 0;i<rollingDice.length;i++){
             rollingDice[i].src= `diceImages/dice-face-${d6Array[i]}.png`
@@ -31,18 +34,30 @@ rollButton.addEventListener("click",()=>{
 
 for(let individualDice of rollingDice){
     individualDice.addEventListener('click',(event)=>{
-        if(conditionCount === 1){
+        if(conditionCount === 1&&maxRerollCount<=1){
             if(!event.target.classList.contains("selected")){
                 event.target.classList.add("selected")
                 reRollArray.push(parseInt(event.target.src.slice(46,-4)))
                 console.log(reRollArray)
             }
         }
+        rerollCount++
     })
 }
 //builds an array for the reroll button to sort through and remove from the original array
 
-
+rerollButton.addEventListener("click",()=>{
+    d6Array = replaceItemInArray(d6Array,reRollArray)
+    if(rerollCount>0&&conditionCount===1&&maxRerollCount===0||maxRerollCount===1){
+        for(let i = 0;i<rollingDice.length;i++){
+            rollingDice[i].src= `diceImages/dice-face-${d6Array[i]}.png`
+            rollingDice[i].classList.remove("selected")
+        }
+        maxRerollCount++
+        rerollCount = 0
+    }
+    console.log(d6Array)
+})
 
 
 
@@ -52,8 +67,15 @@ function randomD6result(){
     return Math.ceil(Math.random()*6)
 }
 
-
-
+function replaceItemInArray(originalArr,filterArr){
+    for(let item of originalArr){
+        if(filterArr.indexOf(item) !== -1){
+            originalArr.splice(originalArr.indexOf(item),1,randomD6result())
+            filterArr.splice(filterArr.indexOf(item),1)
+        }
+    }
+    return originalArr
+}
 //true or false functions
 
 
